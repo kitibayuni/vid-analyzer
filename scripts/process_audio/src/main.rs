ae82065de05f1208c99ed549306540f84478e0fb
kitibayuni <phmacaril@gmail.com>
2025-08-31 13:57:55 -0400

    ADD: added chunking as a memory buffer

--- scripts/process_audio/src/main.rs
+++ scripts/process_audio/src/main.rs
@@ -30,6 +30,11 @@ fn main() -> Result<(), Box<dyn std:error::Error>> {
     let samplerate = flac.streaminfo().sample_rate as usize;
 
     // --- INITIATE PYIN --- //
+    // notes: this creates   //
+    // frames that overlap & //
+    // which will be calc'd  //
+    // after generation.     //
+
     let frame_min = 60.0
     let frame_max = 600.0
     let frame_len = (0.025 * samplerate as f64) as usize;
@@ -39,8 +44,29 @@ fn main() -> Result<(), Box<dyn std:error::Error>> {
     
     // pyin executor
     let mut pyin_executor = = PYINExecutor::new(frame_min, frame_max, samplerate as i32, frame_len, window_len, hop_len, resolution);
-
+    
+    // framing, so that the sample is in the middle of the frame
     let framing = Framing::Center(PadMode::Constant(0,0));
 
+    
+    // --- CHUNKING --- //
+    // notes: so that   //
+    // memory will not  //
+    // load the entire  //
+    // audio track at   //
+    // once.            //
+
+    let chunk_sec = 30.0;
+    let chunk_samples = (chunk_sec * samplerate as f64) as usize;
+    let overlap_samples = frame_len;
+    
+    // pre-allocating a vector buffer to hold chunk_samples and overlap_camples
+    let mut chunk_buffer: Vec<f64> = Vec::with_capacity(chunk_samples + overlap_samples);
+    let mut chunk_start_time = 0.0; // mutable to adjust for next chunk
+    let mut csv_writer = Writer::from_path("pitch_output.csv")?; // change for a dynamic name
+                                                                 // generation
+    writer.write_record(&["time_sec", "pitch_hz"])?;
+
+    // write iteration over chunks to process/write time_sec and calculated pitch here ...
 
 }
